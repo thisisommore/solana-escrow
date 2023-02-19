8260549d31adafd021830428427e35e096d72741
thisisommore <ommore501@gmail.com>
2023-02-19 04:51:07 +0000

    Update with new files

---
+++ src/error.rs
@@ -0,0 +1,17 @@
+// inside error.rs
+use solana_program::program_error::ProgramError;
+use thiserror::Error;
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+    #[error("Not Rent Exempt")]
+    NotRentExempt,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
